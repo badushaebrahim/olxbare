b40ae82fc79667b355ffca8bb0936f3e0cd0beeb
BADU\badus <badushaebrahim62@gmail.com>
2022-03-31 10:48:44 +0530

    mmr

--- app/src/main/java/com/example/olx_bare/da.java
+++ app/src/main/java/com/example/olx_bare/da.java
@@ -1,7 +1,7 @@
 package com.example.olx_bare;
 
 public class da {
-    String URL="http://192.168.0.104:81/andro/";
+    String URL="http://192.168.43.106:81/andro/";
     da(String m ){
         System.out.print(m);
     }
